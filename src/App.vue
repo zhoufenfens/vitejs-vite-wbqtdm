1b1c9f257c6a68f592e6e881c8a5b08065793504
zhoufenfens <806098942@qq.com>
2023-01-07 16:27:48 +0800

    增加样式

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
-  <router-view></router-view>
+  <div class='router-view'>
+    <router-view></router-view>
+  </div>
+
+  <div>
+    <a class='link' href='/#/news'>切换到/news</a>
+    
+    <a class='link'  href='/#/news2'>切换到/news2</a>
+  </div>
   <van-button>App.vue里的按钮</van-button>
 </template>
 
@@ -19,5 +27,17 @@
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.link {
+  display: block;
+}
+.router-view {
+  width: 300px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid red;
+  padding: 5px;
 }
 </style>
